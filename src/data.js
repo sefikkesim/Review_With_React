e5b967b28d2c9de4d6475673e7ba458c09b6953f
sefikkesim <sefikkesim@gmail.com>
2021-12-10 06:41:59 +0200

    Review with React

---
+++ src/data.js
@@ -0,0 +1,84 @@
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+  {
+    id: 5,
+    name: "Jane Teyler",
+    job: "intern",
+    image:
+      "https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, recusandae! Veritatis nulla minima provident perspiciatis quae atque quibusdam, vitae magni.",
+  },
+  {
+    id: 6,
+    name: "bill anderson",
+    job: "UI Designer ",
+    image:
+      "https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, recusandae! Veritatis nulla minima provident perspiciatis quae atque quibusdam, vitae magni.",
+  },
+  {
+    id: 7,
+    name: "Jack Nick",
+    job: "UX Designer",
+    image:
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, recusandae! Veritatis nulla minima provident perspiciatis quae atque quibusdam, vitae magni.",
+  },
+  {
+    id: 8,
+    name: "Jakob Owens",
+    job: "Photographer",
+    image:
+      "https://images.unsplash.com/photo-1493863641943-9b68992a8d07?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=858&q=80",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, recusandae! Veritatis nulla minima provident perspiciatis quae atque quibusdam, vitae magni.",
+  },
+  {
+    id: 9,
+    name: "Laura Anderson",
+    job: "Web Designer",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=400&q=60",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, recusandae! Veritatis nulla minima provident perspiciatis quae atque quibusdam, vitae magni. ",
+  },
+  {
+    id: 10,
+    name: "Antony McCharty",
+    job: "Fullstack Developer",
+    image:
+      "https://images.unsplash.com/photo-1540569014015-19a7be504e3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, recusandae! Veritatis nulla minima provident perspiciatis quae atque quibusdam, vitae magni.",
+  },
+];
+
+export default reviews;
